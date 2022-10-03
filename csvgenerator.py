0e298eab0d0b7da2902beabb9e86909fee13c4b7
mun777 <92113843+mun777@users.noreply.github.com>
2022-10-03 14:07:04 -0500

    Update csvgenerator.py
    handle dates

--- csvgenerator.py
+++ csvgenerator.py
@@ -1,10 +1,8 @@
-
 import csv
 import random as r
-
+import datetime as d
 number_of_records = input("Number of data needed ")
 number_of_records = int(number_of_records)
-
 
 with open('313project.csv', mode='w') as file:
   file_writer = csv.writer(file,
@@ -12,11 +10,13 @@ with open('313project.csv', mode='w') as file:
                            quotechar='"',
                            quoting=csv.QUOTE_MINIMAL)
 
-  file_writer.writerow(['YYYY-MM-DD', 'Net Sales', 'Orders'])
-
+  file_writer.writerow(['YYYY-MM-DD', 'net sales-currency', 'num orders-int'])
+  date = d.datetime(2020, 2, 20)
+  
   for i in range(number_of_records):
+    date += d.timedelta(days=1)
     file_writer.writerow([
-      r.randrange(0, 8, 2),
+      date,
       round(r.uniform(500.00, 2000.00), 2),
       r.randrange(10, 400)
     ])
