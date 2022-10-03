97029827df595da60d14bbf8470193e156eb5b8c
Yingchen Dong <yc.dong@tamu.edu>
2022-10-03 14:44:52 -0500

    generate csv file for saleshistory

--- csvgenerator.py
+++ csvgenerator.py
@@ -4,14 +4,14 @@ import datetime as d
 number_of_records = input("Number of data needed ")
 number_of_records = int(number_of_records)
 
-with open('313project.csv', mode='w') as file:
+with open('315project2.csv', mode='w') as file:
   file_writer = csv.writer(file,
                            delimiter=',',
                            quotechar='"',
                            quoting=csv.QUOTE_MINIMAL)
 
   file_writer.writerow(['YYYY-MM-DD', 'net sales-currency', 'num orders-int'])
-  date = d.datetime(2020, 2, 20)
+  date = d.date(2020, 2, 20)
   
   for i in range(number_of_records):
     date += d.timedelta(days=1)
