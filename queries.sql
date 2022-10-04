506129f9a139d66cec207ecabaee37e771e2ca81
muneebhashmi339 <17561+muneebhashmi339@users.noreply.github.tamu.edu>
2022-10-04 17:57:18 -0500

    SQL file with 15 queries, can run all at once

---
+++ queries.sql
@@ -0,0 +1,44 @@
+\echo SELECT * FROM saleshistory
+SELECT * FROM saleshistory;
+
+\echo SELECT * FROM inventory
+SELECT * FROM inventory;
+
+\echo SELECT item FROM inventory
+SELECT item FROM inventory;
+
+\echo SELECT date FROM saleshistory
+SELECT date FROM saleshistory;
+
+\echo SELECT AVG(netsales) FROM saleshistory
+SELECT AVG(netsales) FROM saleshistory;
+
+\echo SELECT COUNT(item) FROM inventory
+SELECT COUNT(item) FROM inventory;
+
+\echo SELECT * FROM inventory WHERE category='entree' AND quantity=100 LIMIT 5
+SELECT * FROM inventory WHERE category='entree' AND quantity=100 LIMIT 5;
+
+\echo SELECT * FROM saleshistory WHERE date >= '2022-01-13'
+SELECT * FROM saleshistory WHERE date >= '2022-01-13';
+
+\echo SELECT netsales, numorders FROM saleshistory WHERE numorders BETWEEN '300' AND '500'
+SELECT netsales, numorders FROM saleshistory WHERE numorders BETWEEN '300' AND '500';
+
+\echo SELECT MAX(numorders) FROM saleshistory
+SELECT MAX(numorders) FROM saleshistory;
+
+\echo SELECT SUM(netsales) FROM saleshistory
+SELECT SUM(netsales) FROM saleshistory;
+
+\echo SELECT item FROM inventory WHERE item like '%sauce%'
+SELECT item FROM inventory WHERE item like '%sauce%';
+
+\echo SELECT id FROM inventory
+SELECT id FROM inventory;
+
+\echo SELECT MAX(quantity) FROM inventory
+SELECT MAX(quantity) FROM inventory;
+
+\echo SELECT * FROM saleshistory LIMIT 2
+SELECT * FROM saleshistory LIMIT 2;
