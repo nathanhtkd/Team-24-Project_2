0733173fe0164c65de6c7a413b072c95d53b41ec
Afreen <a3pmfatima@gmail.com>
2022-10-05 13:51:37 +0000

    added additional queries

--- queries.sql
+++ queries.sql
@@ -4,14 +4,17 @@ SELECT * FROM saleshistory;
 \echo SELECT * FROM inventory
 SELECT * FROM inventory;
 
+\echo SELECT* FROM itemizedhistory;
+SELECT* FROM itemizedhistory;
+
 \echo SELECT item FROM inventory
 SELECT item FROM inventory;
 
-\echo SELECT date FROM saleshistory
-SELECT date FROM saleshistory;
+\echo SELECT date FROM itemizedhistory
+SELECT date FROM itemizedhistory;
 
-\echo SELECT AVG(netsales) FROM saleshistory
-SELECT AVG(netsales) FROM saleshistory;
+\echo SELECT AVG(price) FROM itemizedhistory
+SELECT AVG(price) FROM itemizedhistory;
 
 \echo SELECT COUNT(item) FROM inventory
 SELECT COUNT(item) FROM inventory;
@@ -19,8 +22,8 @@ SELECT COUNT(item) FROM inventory;
 \echo SELECT * FROM inventory WHERE category='entree' AND quantity=100 LIMIT 5
 SELECT * FROM inventory WHERE category='entree' AND quantity=100 LIMIT 5;
 
-\echo SELECT * FROM saleshistory WHERE date >= '2022-01-13'
-SELECT * FROM saleshistory WHERE date >= '2022-01-13';
+\echo SELECT * FROM itemizedhistory WHERE date >= '2022-09-20';
+SELECT * FROM itemizedhistory WHERE date >= '2022-09-20';
 
 \echo SELECT netsales, numorders FROM saleshistory WHERE numorders BETWEEN '300' AND '500'
 SELECT netsales, numorders FROM saleshistory WHERE numorders BETWEEN '300' AND '500';
@@ -28,7 +31,7 @@ SELECT netsales, numorders FROM saleshistory WHERE numorders BETWEEN '300' AND '
 \echo SELECT MAX(numorders) FROM saleshistory
 SELECT MAX(numorders) FROM saleshistory;
 
-\echo SELECT SUM(netsales) FROM saleshistory
+\echo SELECT SUM(price) FROM itemizedhistory
 SELECT SUM(netsales) FROM saleshistory;
 
 \echo SELECT item FROM inventory WHERE item like '%sauce%'
@@ -40,7 +43,7 @@ SELECT id FROM inventory;
 \echo SELECT MAX(quantity) FROM inventory
 SELECT MAX(quantity) FROM inventory;
 
-\echo SELECT * FROM saleshistory LIMIT 2
+\echo SELECT * FROM itemizedhistory LIMIT 2
 SELECT * FROM saleshistory LIMIT 2;
 
 \echo SELECT * FROM saleshistory WHERE netsales > 4000
